e1ab6b4cba6ae291946280f1ef23ab301d7c58f4
BenoitGaudieri <benalek@gmail.com>
2020-12-20 16:45:08 +0100

    🎨 scrollableList style

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,12 +1,19 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Alert, StyleSheet, Text, View } from "react-native";
+import { Alert, ScrollView, StyleSheet, Text, View } from "react-native";
 import Card from "../components/Card";
 import MainButton from "../components/MainButton";
 import NumberContainer from "../components/NumberContainer";
 import DefaultStyles from "../constants/default-styles";
 // Icons from Ionicons
 import { Ionicons } from "@expo/vector-icons";
+import BodyText from "../components/BodyText";
 
+/**
+ * generates random number with a min and max val excluding val
+ * @param {int} min
+ * @param {int} max
+ * @param {int} exclude
+ */
 const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -18,21 +25,30 @@ const generateRandomBetween = (min, max, exclude) => {
     }
 };
 
+const renderListItem = (value, numOfRounds) => (
+    <View key={value} style={styles.listItem}>
+        <BodyText>Round: #{numOfRounds} </BodyText>
+        <BodyText>{value}</BodyText>
+    </View>
+);
+
 const GameScreen = (props) => {
-    const [currentGuess, setCurrentGuess] = useState(
-        generateRandomBetween(1, 100, props.userChoice)
-    );
+    const initialGuess = generateRandomBetween(1, 100, props.userChoice);
 
-    const [rounds, setRounds] = useState(0);
-
+    /**
+     * States
+     */
+    const [currentGuess, setCurrentGuess] = useState(initialGuess);
+    const [pastGuesses, setpastGuesses] = useState([initialGuess]);
     const currentLow = useRef(1);
     const currentHigh = useRef(100);
 
+    // registers the props so they can be used with useEffect
     const { userChoice, onGameOver } = props;
 
     useEffect(() => {
         if (currentGuess === userChoice) {
-            onGameOver(rounds);
+            onGameOver(pastGuesses.length);
         }
         // check if these value change and run the use effect
     }, [currentGuess, userChoice, onGameOver]);
@@ -50,14 +66,16 @@ const GameScreen = (props) => {
         if (direction === "lower") {
             currentHigh.current = currentGuess;
         } else {
-            currentLow.current = currentGuess;
+            currentLow.current = currentGuess + 1;
         }
         const nextNumber = generateRandomBetween(
             currentLow.current,
-            currentHigh.current
+            currentHigh.current,
+            currentGuess
         );
         setCurrentGuess(nextNumber);
-        setRounds((curRounds) => curRounds + 1);
+        // setpastGuesses((curpastGuesses) => curpastGuesses + 1);
+        setpastGuesses((curPastGuesses) => [nextNumber, ...curPastGuesses]);
     };
 
     return (
@@ -72,6 +90,14 @@ const GameScreen = (props) => {
                     <Ionicons name="md-add" size={24} color="white" />
                 </MainButton>
             </Card>
+
+            <View style={styles.listContainer}>
+                <ScrollView contentContainerStyle={styles.list}>
+                    {pastGuesses.map((guess, index) =>
+                        renderListItem(guess, pastGuesses.length - index)
+                    )}
+                </ScrollView>
+            </View>
         </View>
     );
 };
@@ -90,5 +116,29 @@ const styles = StyleSheet.create({
         marginTop: 20,
         width: 300,
         maxWidth: "80%",
+    },
+
+    listContainer: {
+        // flex 1 is needed on android otherwise scrollable won't work
+        flex: 1,
+        width: "80%",
+    },
+
+    list: {
+        // with scrollable list it's better to use flexGrow
+        flexGrow: 1,
+        alignItems: "center",
+        justifyContent: "flex-end",
+    },
+
+    listItem: {
+        borderColor: "#ccc",
+        borderWidth: 1,
+        padding: 15,
+        marginVertical: 10,
+        backgroundColor: "white",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "60%",
     },
 });
